cf3e0fca121ee3f3561f3341f5bfe33ec04fc075
gdm <seankim@Seans-iMac.local>
2016-02-27 13:39:46 -0500

    add reset_index example

--- tests/pandas_options_example.py
+++ tests/pandas_options_example.py
@@ -40,6 +40,7 @@ print back_month_puts.head()
 # convenience variables
 fm = front_month_puts
 
+
 print_header()
 
 # filter out options by their Vol (Vol is a value in the row)
@@ -47,20 +48,22 @@ min_vol = 10
 min_oi = 50
 
 active_fm_puts = fm[fm.Vol > min_vol]
-print "FM PUTS WITH A MINIMUM OF AT LEAST VOLUME 10"
+print "FM PUTS WITH A MINIMUM OF AT LEAST VOLUME", min_vol
 print "count:", len(active_fm_puts)
 print active_fm_puts.head()
 
-
+# filter by Vol and Open Interest
 print_header()
 active_fm_puts_with_oi = fm[(fm.Vol > min_vol) & (fm.Open_Int > min_oi)]
-print "FM PUTS WITH A MINIMUM OF AT LEAST VOLUME 10 AND OI > 50"
+print "FM PUTS WITH A MINIMUM OF AT LEAST VOLUME", min_vol,\
+        " AND OI >", min_oi
 print "count:", len(active_fm_puts_with_oi)
 print active_fm_puts_with_oi.head()
 
 
+# filter by Vol or Open Interest
 print_header()
-active_fm_puts_or_oi = = fm[(fm.Vol > min_vol) | (fm.Open_Int > min_oi)]
+active_fm_puts_or_oi = fm[(fm.Vol > min_vol) | (fm.Open_Int > min_oi)]
 print "FM PUTS WITH A MINIMUM OF AT LEAST VOLUME 10 OR OI > 50"
 print "count:", len(active_fm_puts_or_oi)
 print active_fm_puts_or_oi.head()
@@ -71,6 +74,15 @@ print_header()
 # Strike is the first index of the levels
 itm_fm_puts = fm[fm.index.levels[0] > options.underlying_price]
 print "itm front month puts"
+print "count:", len(itm_fm_puts)
+print itm_fm_puts.head()
+
+# reindexed dataframe(all levels in index become a col)
+ri_fm = fm.reset_index()
+ri_itm_fm_puts = ri_fm[ri_fm.Strike > options.underlying_price]
+print "itm front month puts"
+print "count:", len(ri_itm_fm_puts)
+print ri_itm_fm_puts.head()
 
 
 
