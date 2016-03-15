af9ef81b247a6beacae5f7a7cacb21bc7d16e3eb
kimgoon <unk@unk.com>
2016-03-14 23:49:30 -0400

    add logic for printing evens

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python
 
-print "Hello"
+# example to show how to use generators
+
+# define a method to create evens
+def get_evens(max_num):
+    for i in xrange(max_num+1):
+        if i % 2 == 0:
+            yield i
+
+# call function with an upper limit
+for val in get_evens(10):
+    print val
